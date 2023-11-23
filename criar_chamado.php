c0cbbc9a699e9216354fde23bbb9cc1a995e14bb
Davi Mattos <127062546+zollakdev@users.noreply.github.com>
2023-11-23 20:22:37 -0300

    Update criar_chamado.php

--- criar_chamado.php
+++ criar_chamado.php
@@ -26,8 +26,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email'])) {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'astrosteamtoken@gmail.com';
-    $mail->Password = 'vxqr wsxn lzix ljel';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
